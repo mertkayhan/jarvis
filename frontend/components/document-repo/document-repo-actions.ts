ef7106f606e83b9549f01dab9ce65a9795553892
mertkayhan <mertkayhan.1993@gmail.com>
2025-05-09 15:19:12 +0200

    refactor

--- frontend/components/document-repo/document-repo-actions.ts
+++ frontend/components/document-repo/document-repo-actions.ts
@@ -2,6 +2,7 @@
 
 import { UserDocument } from "@/lib/types";
 import { getToken } from "../chat/chat-actions";
+import { callBackend } from "@/lib/utils";
 
 export interface ListDocumentsResp {
     docs: UserDocument[] | null
@@ -9,20 +10,12 @@ export interface ListDocumentsResp {
 
 export async function listDocuments(userId: string) {
     console.log("list documents", userId);
-    const backendUrl = process.env.BACKEND_URL;
-    const token = await getToken();
-    const resp = await fetch(
-        `${backendUrl}/api/v1/users/${userId}/docs`,
+    const data = await callBackend(
         {
-            method: "GET",
-            headers: { "Authorization": `Bearer ${token}` }
+            endpoint: `/api/v1/users/${userId}/docs`,
+            method: "GET"
         }
     );
-    const data = await resp.json();
-    if (!resp.ok) {
-        console.error("failed to fetch user docs", data);
-        throw new Error("failed to fetch user docs");
-    }
     return {docs: data?.docs.map((d: Record<string, any>) => {
         return {...d, createdAt: new Date(d.createdAt)}
     })} as ListDocumentsResp;
@@ -34,19 +27,11 @@ interface DeleteDocumentResp {
 
 export async function deleteDocument(userId:string, docId: string) {
     console.log("delete document", docId);
-    const backendUrl = process.env.BACKEND_URL;
-    const token = await getToken();
-    const resp = await fetch(
-        `${backendUrl}/api/v1/users/${userId}/docs/${docId}`,
+    const data = await callBackend(
         {
+            endpoint: `/api/v1/users/${userId}/docs/${docId}`,
             method: "DELETE",
-            headers: { "Authorization": `Bearer ${token}` }
         }
     );
-    const data = await resp.json();
-    if (!resp.ok) {
-        console.error("failed to delete user doc", data);
-        throw new Error("failed to delete user doc");
-    }
     return data as DeleteDocumentResp;
 }
--- frontend/components/model-selection/model-selection-actions.ts
+++ frontend/components/model-selection/model-selection-actions.ts
@@ -1,5 +1,6 @@
 "use server"
 
+import { callBackend } from "@/lib/utils";
 import { getToken } from "../chat/chat-actions";
 
 interface GetAvailableModelsResp {
@@ -7,16 +8,12 @@ interface GetAvailableModelsResp {
 }
 
 export async function getAvailableModels(userId: string) {
-    const backendUrl = process.env.BACKEND_URL;
-    const token = await getToken();
-    const resp = await fetch(
-        `${backendUrl}/api/v1/users/${userId}/models`,
+    const data = await callBackend(
         {
+            endpoint: `/api/v1/users/${userId}/models`,
             method: "GET",
-            headers: { "Authorization": `Bearer ${token}` }
         }
     );
-    const data = await resp.json();
     return { models: data.models } as GetAvailableModelsResp;
 }
 
@@ -26,20 +23,10 @@ interface GetUserModelResp {
 
 export async function getUserModel(userId: string) {
     console.log("get user model:", userId);
-    const backendUrl = process.env.BACKEND_URL;
-    const token = await getToken();
-    const resp = await fetch(
-        `${backendUrl}/api/v1/users/${userId}/model-selection`,
-        {
-            method: "GET",
-            headers: { "Authorization": `Bearer ${token}` }
-        }
-    );
-    const data = await resp.json();
-    if (!resp.ok) {
-        console.error("failed to get user model", data);
-        throw new Error("failed to get user model");
-    }
+    const data = await callBackend({
+        endpoint: `/api/v1/users/${userId}/model-selection`,
+        method: "GET",
+    });
     return { modelName: data.model } as GetUserModelResp;
 
 }
@@ -50,20 +37,10 @@ interface SetUserModelResp {
 
 export async function setUserModel(userId: string, modelName: string) {
     console.log("set user model", userId, modelName);
-    const backendUrl = process.env.BACKEND_URL;
-    const token = await getToken();
-    const resp = await fetch(
-        `${backendUrl}/api/v1/users/${userId}/model-selection`,
-        {
-            method: "POST",
-            headers: { "Authorization": `Bearer ${token}`, "Content-Type": "application/json" },
-            body: JSON.stringify({ "model_name": modelName }),
-        }
-    );
-    const data = await resp.json();
-    if (!resp.ok) {
-        console.error("failed to set user model", data);
-        throw new Error("failed to set user model");
-    }
+    const data = await callBackend({
+        endpoint: `/api/v1/users/${userId}/model-selection`,
+        method: "POST",
+        body: { "model_name": modelName }
+    });
     return { modelName: data.model } as SetUserModelResp;
 }
